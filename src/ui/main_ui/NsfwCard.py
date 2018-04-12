6a00d5167099050170fde0880351d26ec6c84402
sclMAX <maxisoftman@gmail.com>
2018-04-12 18:35:31 -0300

    desarrollo

--- src/ui/main_ui/NsfwCard.py
+++ src/ui/main_ui/NsfwCard.py
@@ -1,13 +1,24 @@
 from pathlib import Path
 import webbrowser
+import cv2 as cv
 from PyQt5 import QtGui, QtWidgets, QtCore
+
 
 class NsfwCard(QtWidgets.QFrame):
     data: dict
     base_path: str
 
-    #Signals
+    # Signals
     remove_me: QtCore.pyqtSignal = QtCore.pyqtSignal(object)
+
+    # Gif and Video
+    cap = None
+    frames: int = 0
+    currentFrame: int = 0
+    frameIncremento: int = 1
+    nextFrame: int = 1
+    __timer: QtCore.QTimer = QtCore.QTimer()
+
 
     def __init__(self, parent, media_item, width: int, height: int, base_path: str):
         super().__init__(parent)
@@ -17,14 +28,10 @@ class NsfwCard(QtWidgets.QFrame):
         self.self_btnOpen.clicked.connect(self.btnOpen_click)
         self.self_btnRemove.clicked.connect(self.btnRemove_click)
 
-    def __del__(self):
-        print('Delete')
-        self.cleanUp()
-
     def getScore(self):
         comment: str = self.data.get('Comments')
         score: float = float(comment)
-        return score or 0
+        return score
 
     def getFilePath(self):
         file_path: str = self.data.get('RelativeFilePath')
@@ -34,11 +41,67 @@ class NsfwCard(QtWidgets.QFrame):
             if Path(self.base_path) != media_base_path.parent:
                 file_path = str(Path(self.base_path).joinpath(file_path))
             return file_path
-        return 'Error RelativeFilePath'
+        return ''
+
+    def getImage(self):
+        file_path = self.getFilePath()
+        if file_path:
+            file_type = self.data.get('FileType')
+            file_Extension = self.data.get('FileExtension')
+            if file_type and 'video' in file_type:
+                return self.__processVideo(file_path)
+            if file_Extension and 'gif' in file_Extension:
+                return self.__processGif(file_path)
+            return QtGui.QPixmap(file_path)
+        return QtGui.QPixmap('icons/noimage.gif')
+
+    def __processGif(self, file: str):
+        import imageio
+        try:
+            n = imageio.mimread(file)
+            self.frames = len(n)
+        except:
+            return QtGui.QPixmap(file)
+        self.frameIncremento = 1
+        self.nextFrame = 2
+        self.cap = cv.VideoCapture(file)
+        self.__timer.setInterval(500)
+        self.__timer.timeout.connect(self.__play)
+        self.__timer.start()
+        return QtGui.QPixmap(file)
+        
+    def __processVideo(self, file: str):
+        self.cap = cv.VideoCapture(file)
+        self.frames = abs(self.cap.get(cv.CAP_PROP_FRAME_COUNT))
+        self.frameIncremento = abs(self.cap.get(cv.CAP_PROP_FPS))
+        self.__timer.setInterval(100)
+        self.__timer.timeout.connect(self.__play)
+        self.__timer.start()
+        return QtGui.QPixmap('icons/noimage.gif')
+
+    def __play(self):
+        if self.cap and self.cap.isOpened:
+            self.nextFrame += self.frameIncremento
+            if self.nextFrame >= self.frames:
+                self.nextFrame = self.frameIncremento
+            self.cap.set(1, self.nextFrame - 1)
+            ok, frame = self.cap.read()
+            if ok:
+                frame = cv.resize(frame, (200, 200))
+                height, width, _ = frame.shape
+                bytesPerLine = 3 * width
+                cv.cvtColor(frame, cv.COLOR_BGR2RGB, frame)
+                img = QtGui.QImage(frame.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888)
+                self.self_image.setPixmap(QtGui.QPixmap(img))
+                self.self_image.repaint()
+            else:
+                self.cap.release()
+                self.cap = cv.VideoCapture(self.getFilePath())
 
     def __setup(self, w: int, h: int):
         self.resize(w, h)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
@@ -48,15 +111,18 @@ class NsfwCard(QtWidgets.QFrame):
         self.setFrameShadow(QtWidgets.QFrame.Plain)
         self.setLineWidth(2)
         self.verticalLayout = QtWidgets.QVBoxLayout(self)
-        self.verticalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.verticalLayout.setSizeConstraint(
+            QtWidgets.QLayout.SetDefaultConstraint)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setSpacing(2)
         self.verticalLayout.setObjectName("verticalLayout")
         self.horizontalFrame = QtWidgets.QFrame(self)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.horizontalFrame.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.horizontalFrame.sizePolicy().hasHeightForWidth())
         self.horizontalFrame.setSizePolicy(sizePolicy)
         self.horizontalFrame.setObjectName("horizontalFrame")
         self.self_buttons = QtWidgets.QHBoxLayout(self.horizontalFrame)
@@ -64,67 +130,78 @@ class NsfwCard(QtWidgets.QFrame):
         self.self_buttons.setSpacing(2)
         self.self_buttons.setObjectName("self_buttons")
         self.self_btnOpen = QtWidgets.QToolButton(self.horizontalFrame)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.self_btnOpen.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.self_btnOpen.sizePolicy().hasHeightForWidth())
         self.self_btnOpen.setSizePolicy(sizePolicy)
         self.self_btnOpen.setText("")
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("icons/icon-search.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("icons/icon-search.png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.self_btnOpen.setIcon(icon)
         self.self_btnOpen.setIconSize(QtCore.QSize(16, 16))
         self.self_btnOpen.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
         self.self_btnOpen.setArrowType(QtCore.Qt.NoArrow)
-        self.self_btnOpen.setObjectName("self_btnOpen")
+        self.self_btnOpen.setToolTip('Abrir imagen.')
         self.self_buttons.addWidget(self.self_btnOpen)
         self.self_btnRemove = QtWidgets.QToolButton(self.horizontalFrame)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.self_btnRemove.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.self_btnRemove.sizePolicy().hasHeightForWidth())
         self.self_btnRemove.setSizePolicy(sizePolicy)
         self.self_btnRemove.setText("")
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("icons/icon-circledelete.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon1.addPixmap(QtGui.QPixmap("icons/icon-circledelete.png"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.self_btnRemove.setIcon(icon1)
         self.self_btnRemove.setIconSize(QtCore.QSize(16, 16))
-        self.self_btnRemove.setObjectName("self_btnRemove")
+        self.self_btnRemove.setToolTip('Quitar del reporte.')
         self.self_buttons.addWidget(self.self_btnRemove)
-        self.verticalLayout.addWidget(self.horizontalFrame, 0, QtCore.Qt.AlignTop)
+        self.verticalLayout.addWidget(
+            self.horizontalFrame, 0, QtCore.Qt.AlignTop)
         self.self_image = QtWidgets.QLabel(self)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.self_image.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.self_image.sizePolicy().hasHeightForWidth())
         self.self_image.setSizePolicy(sizePolicy)
         self.self_image.setText("")
-        file_path = self.getFilePath()
-        if file_path != 'Error RelativeFilePath':
-            self.self_image.setPixmap(QtGui.QPixmap(file_path))
-        else:
-            self.self_image.setPixmap(QtGui.QPixmap('icons/noimage.gif'))
+        self.self_image.setPixmap(self.getImage())
         self.self_image.setScaledContents(True)
-        self.self_image.setToolTip(file_path)
+        self.self_image.setToolTip(self.getFilePath())
         self.self_image.setAlignment(QtCore.Qt.AlignCenter)
         self.self_image.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
         self.verticalLayout.addWidget(self.self_image)
         self.self_score = QtWidgets.QProgressBar(self)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.self_score.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.self_score.sizePolicy().hasHeightForWidth())
         self.self_score.setSizePolicy(sizePolicy)
         font = QtGui.QFont()
         font.setPointSize(5)
         font.setBold(True)
         font.setWeight(75)
         self.self_score.setFont(font)
-        self.self_score.setValue(self.getScore() * 100)
+        try:
+            score = self.getScore()
+            self.self_score.setValue(score * 100)
+        except ValueError:
+            self.self_score.setVisible(False)
         self.self_score.setAlignment(QtCore.Qt.AlignCenter)
         self.self_score.setTextVisible(True)
-        self.self_score.setObjectName("self_score")
-        self.verticalLayout.addWidget(self.self_score, 0, QtCore.Qt.AlignBottom)
+        self.verticalLayout.addWidget(
+            self.self_score, 0, QtCore.Qt.AlignBottom)
 
     def btnOpen_click(self):
         webbrowser.open_new_tab(self.getFilePath())
--- src/ui/main_ui/UiMain.py
+++ src/ui/main_ui/UiMain.py
@@ -1,20 +1,26 @@
 from pathlib import Path
+import json
 from src.ui.main_ui.ui_main import Ui_MainWindow
-from src.ui.scann_ui.UiScann import DlgScanner, QtWidgets, QtCore
-from src.Nsfw.vic13 import readVICFromFile, getMediaFormVIC
+from src.ui.scann_ui.UiScann import DlgScanner, QtWidgets, QtCore, QtGui
+from src.Nsfw.vic13 import readVICFromFile, getMediaFormVIC, updateMedia
 from src.ui.main_ui.NsfwCard import NsfwCard
 
 class UiMain(QtWidgets.QMainWindow, Ui_MainWindow):
     resized: QtCore.pyqtSignal = QtCore.pyqtSignal()
     dlgScann: DlgScanner
-    vic_file: str = ''
+    vic_file: str
+    save_file: str
     VIC: dict = None
+    #Media Vars
     media: list = []
+    filter_media: list = []
+    undo_media: list = []
+    isChanged: bool = False
     #Filter Vars
     isFilterChange: bool = True
-    filter_media: list = []
     filter_value: float = 0.15
     isFiltered: bool = False
+    isFilterInvert: bool = False
 
     #Cards Vars
     cards_list: list = []
@@ -31,9 +37,26 @@ class UiMain(QtWidgets.QMainWindow, Ui_MainWindow):
         self.slrFiltro.valueChanged.connect(self.pgbFiltro.setValue)
         self.pgbFiltro.valueChanged.connect(self.changeFilterScore)
         self.btnFiltro.clicked.connect(self.setFilterOnOff)
+        self.btnFiltro_Invertir.clicked.connect(self.setInvertFilter)
         self.btnScanner.clicked.connect(self.btnScanner_Click)
         self.btnOpen.clicked.connect(self.btnOpen_Click)
+        self.btnSave.clicked.connect(self.btnSave_click)
+        self.btnUndo.clicked.connect(self.undo)
         self.resized.connect(self.__updateView)
+
+    oldDir = 0
+    whellCount = 0
+    def wheelEvent(self, event: QtGui.QWheelEvent):
+        direction = event.angleDelta().y()
+        if direction != self.oldDir:
+            self.oldDir = direction
+            self.whellCount = 0
+        self.whellCount += 1
+        if self.VIC and self.whellCount > 2:
+            page: int = self.oldDir // 120
+            if self.setCurrentPage(self.current_page - page):
+                self.__updateView()
+            self.whellCount = 0
 
     def resizeEvent(self, event):
         self.setCurrentPage(1)
@@ -43,8 +66,17 @@ class UiMain(QtWidgets.QMainWindow, Ui_MainWindow):
     def changeFilterScore(self):
         fv = self.pgbFiltro.value()
         self.filter_value = fv /100
-        self.lblFiltro.setText('%d' % (fv) + '%')
+        tf = '<' if self.isFilterInvert else '>'
+        self.lblFiltro.setText('%s%d%s' % (tf, fv, '%'))
         self.isFilterChange = True
+
+    def setInvertFilter(self):
+        self.isFilterInvert = not self.isFilterInvert
+        self.btnFiltro_Invertir.setChecked(self.isFilterInvert)
+        self.changeFilterScore()
+        if self.isFiltered:
+            self.isFilterChange = True
+            self.__updateView()
 
     def setFilterOnOff(self):
         self.isFiltered = not self.isFiltered
@@ -60,9 +92,22 @@ class UiMain(QtWidgets.QMainWindow, Ui_MainWindow):
             return True
         return False
 
+    def undo(self):
+        if self.undo_media:
+            data = self.undo_media.pop()
+            if not data in self.media:
+                self.media.append(data)
+            if self.isFiltered and not data in self.filter_media:
+                self.filter_media.append(data)
+            if not self.undo_media:
+                self.isChanged = False
+                self.btnSave.setEnabled(self.isChanged)
+            self.__updateView()
+        self.btnUndo.setEnabled(len(self.undo_media) > 0)
+
     def update_lbls(self):
         self.lblPages.setText('%d/%d' % (self.current_page, self.total_pages))
-        self.lblSelCount.setText('%d/%d' % (self.viewCards, len(self.filter_media)) + ('-[F]' if self.isFiltered else ''))
+        self.lblSelCount.setText('%d/%d' % (self.viewCards, len(self.filter_media)) + (' [F]' if self.isFiltered else ''))
 
     def btnListUp_click(self):
         if self.setCurrentPage(self.current_page - 1):
@@ -80,39 +125,72 @@ class UiMain(QtWidgets.QMainWindow, Ui_MainWindow):
         self.vic_file, _ = QtWidgets.QFileDialog.getOpenFileName(self, caption='Abrir Reporte...', filter='*.json')
         self.__loadReportFile()
 
+    def btnSave_click(self):
+        self.save_file, _ = QtWidgets.QFileDialog.getSaveFileName(self, caption='Gardar Reporte...', filter='*.json')
+        self.saveReport()
+
     def setStatus(self, msg):
         self.lblOpenFolder.setText(msg.msg)
         self.lblOpenFolder.repaint()
 
+    def setProgressStatus(self, txt: str):
+        self.lblProgress.setText(txt)
+        self.lblProgress.repaint()
+
     def __filterMedia(self):
         if not self.isFiltered:
-            self.filter_media = self.media
+            self.filter_media = self.media.copy()
             self.isFilterChange = True
             return
         if self.isFilterChange:
             self.filter_media = []
+            count: int = 0
+            total: int = len(self.media)
+            self.progressBar.setVisible(True)
+            self.progressBar.setMaximum(total)
             for media in self.media:
-                if float(media['Comments']) >= self.filter_value:
-                    self.filter_media.append(media)
+                self.setProgressStatus('Analizando %d de %d...' % (count, total))
+                count += 1
+                self.progressBar.setValue(count)
+                try:
+                    score = float(media['Comments'])
+                    if (self.isFilterInvert and score <= self.filter_value) or (not self.isFilterInvert and score >= self.filter_value):
+                        self.filter_media.append(media)
+                except ValueError:
+                    continue
+            self.progressBar.setVisible(False)
+            self.setProgressStatus('')
+            self.setCurrentPage(1)
             self.isFilterChange = False
 
     def clearCardList(self):
-        for card in self.cards_list:
-            self.removeCard(card)
+        while self.cards.count() > 0:
+            item = self.cards.takeAt(0)
+            if not item:
+                continue
+            w = item.widget()
+            if w:
+                w.deleteLater()
         self.cards_list.clear()
+        self.cards_list = []
 
     def removeCard(self, card: NsfwCard):
-        self.cards.removeWidget(card)
-        self.cards_list.remove(card)
+        c = self.cards.takeAt(self.cards.indexOf(card))
+        print(c)
+        if card in self.cards_list:
+            self.cards_list.remove(card)
         card.deleteLater()
-        del card
 
     def card_remove_me(self, card: NsfwCard):
-        self.media.remove(card.data)
-        if self.isFiltered:
+        if card.data in self.media:
+            self.media.remove(card.data)
+        if card.data in self.filter_media:
             self.filter_media.remove(card.data)
-        self.removeCard(card)
-        self.update_lbls()
+        self.undo_media.append(card.data)
+        self.isChanged = True
+        self.btnSave.setEnabled(self.isChanged)
+        self.btnUndo.setEnabled(len(self.undo_media) > 0)
+        self.__updateView()
 
     def getPageMedia(self, cards_for_page: int):
         page_media: list = []
@@ -120,38 +198,58 @@ class UiMain(QtWidgets.QMainWindow, Ui_MainWindow):
         end: int = start + cards_for_page
         if end > len(self.filter_media):
             end = len(self.filter_media)
+        self.progressBar.setVisible(True)
+        self.progressBar.setMaximum(end)
+        self.progressBar.setMinimum(start)
         for idx in range(start, end):
             page_media.append(self.filter_media[idx])
+            self.progressBar.setValue(idx)
+        self.progressBar.setVisible(False)
         return page_media
 
     def __updateView(self):
         if not self.VIC:
+            self.clearCardList()
             return
-        self.__filterMedia()
-        if not self.filter_media:
-            return
-        col, row, cardW, cardH = (0, 0, 200, 200)
-        width, height = (self.listView.width(), self.listView.height())
-        colums, rows = (round(width / (cardW + 10)), round(height / (cardH + 10)))
-        cards_for_page = colums * rows
-        complet_pages, incomplet_page = divmod(len(self.filter_media), cards_for_page)
-        self.total_pages = complet_pages + 1 if(incomplet_page > 0)else 0
-        page_media = self.getPageMedia(cards_for_page)
-        self.clearCardList()
-        base_path = str(Path(self.vic_file).parent)
-        for item in page_media:
-            item = NsfwCard(self.listView, item, cardW, cardH, base_path)
-            item.remove_me.connect(self.card_remove_me)
-            self.cards_list.append(item)
-            if col >= colums:
-                col = 0
-                row += 1
-            self.cards.addWidget(item, row, col)
-            col += 1
-        self.viewCards = self.current_page * cards_for_page
-        if self.viewCards > len(self.filter_media):
-            self.viewCards = len(self.filter_media)
-        self.update_lbls()
+        try:
+            self.__filterMedia()
+            if not self.filter_media:
+                self.clearCardList()
+                return
+            self.listView.setEnabled(False)
+            col, row, cardW, cardH = (0, 0, 200, 200)
+            width, height = (self.listView.width(), self.listView.height())
+            colums, rows = (round(width / (cardW + 10)), round(height / (cardH + 10)))
+            cards_for_page = colums * rows
+            complet_pages, incomplet_page = divmod(len(self.filter_media), cards_for_page)
+            self.total_pages = complet_pages + 1 if(incomplet_page > 0)else 0
+            page_media = self.getPageMedia(cards_for_page)
+            self.clearCardList()
+            base_path = str(Path(self.vic_file).parent)
+            self.progressBar.setVisible(True)
+            total: int = len(page_media)
+            self.progressBar.setMaximum(total)
+            count: int = 0
+            for item in page_media:
+                count += 1
+                self.setProgressStatus('Creando muestra %d de %d...' % (count, total))
+                item = NsfwCard(self.listView, item, cardW, cardH, base_path)
+                item.remove_me.connect(self.card_remove_me)
+                self.cards_list.append(item)
+                if col >= colums:
+                    col = 0
+                    row += 1
+                self.cards.addWidget(item, row, col)
+                col += 1
+                self.progressBar.setValue(count)
+            self.progressBar.setVisible(False)
+            self.setProgressStatus('')
+            self.viewCards = self.current_page * cards_for_page
+            if self.viewCards > len(self.filter_media):
+                self.viewCards = len(self.filter_media)
+            self.update_lbls()
+        finally:
+            self.listView.setEnabled(True)
 
     def __loadReportFile(self):
         if self.vic_file:
@@ -159,3 +257,15 @@ class UiMain(QtWidgets.QMainWindow, Ui_MainWindow):
             self.VIC = readVICFromFile(self.vic_file)
             self.media = getMediaFormVIC(self.VIC)
             self.__updateView()
+
+    def saveReport(self):
+        if self.save_file:
+            try:
+                self.setEnabled(False)
+                self.setProgressStatus('Guardando Reporte...')
+                newVIC = self.VIC.copy()
+                updateMedia(newVIC, self.media)
+                json.dump(newVIC, open(self.save_file, 'w'))
+                self.setProgressStatus('Reporte guardado en %s' % self.save_file)
+            finally:
+                self.setEnabled(True)
